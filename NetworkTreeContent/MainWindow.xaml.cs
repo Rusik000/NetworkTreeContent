59b5f896d5b3f2538a1f7fbd9dcb22a83e6d4785
Must_nu73 <Must_nu73@STHQ0124-13>
2021-12-18 16:47:32 +0400

    Add project files.

---
+++ NetworkTreeContent/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+
+namespace NetworkTreeContent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Count { get; set; } = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string urlAddress = "http://itstep.az";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+                if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream,
+                        Encoding.GetEncoding(response.CharacterSet));
+                string data = readStream.ReadToEnd();
+                response.Close();
+                readStream.Close();
+
+                for (int i = 0; i < data.Length; i++)
+                {
+
+                }
+
+
+            }
+
+
+        }
+
+    }
+}
+
